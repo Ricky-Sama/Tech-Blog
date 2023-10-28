87f4244d664107f351395ce3778c9f9cbdc1bc1c
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-10-28 16:35:25 -0400

    revised bcryptjs with correct bcrypt module/dependency

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcrypt');
 // console.log("Sequelize version:", require('sequelize').version);
 
 class User extends Model {
   checkPassword(loginPw) {
-    return bcryptjs.compareSync(loginPw, this.password);
+    return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
@@ -41,11 +41,11 @@ User.init(
   {
     hooks: {
       async beforeCreate(newUser) {
-        newUser.password = await bcryptjs.hash(newUser.password, 10);
+        newUser.password = await bcrypt.hash(newUser.password, 10);
         return newUser;
       },
       async beforeUpdate(updatedUser) {
-        updatedUser.password = await bcryptjs.hash(updatedUser.password, 10);
+        updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
         return updatedUser;
       },
     },

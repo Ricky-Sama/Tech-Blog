aba4f1889466b9f783b6c9fc81d389e99bd46865
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-10-28 11:56:01 -0400

    added connection.js and server.js files

---
+++ server.js
@@ -0,0 +1,53 @@
+// Required modules and dependencies
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+
+// Creating express app and setting port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setting up session object with secret, cookie, and store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// Using session middleware with session object
+app.use(session(sess));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Applying to public folders
+app.use(express.static("public"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Applying session middleware with a different session object
+app.use(
+  session({
+    secret: process.env.SECRET,
+    store: new SequelizeStore({ db: sequelize }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Using routes from controller
+app.use(routes);
+
+// Syncing sequelize models with database and starting server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+});
